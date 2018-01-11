a0f34c7db346a8c5df920b3d00d8a2942e121600
ruanHerculano <ruannawe95@gmail.com>
2018-01-10 23:37:43 -0200

    criando a primeira imagem com foto

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'httparty', '~> 0.13.7'
 
 gem 'paperclip', '~> 4.3', '>= 4.3.6'
 
+gem 'mongoid-paperclip', '~> 0.0.10'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,9 @@
 class Image
   include Mongoid::Document
+  include Mongoid::Paperclip
+
   field :description, type: String
+
+  has_mongoid_attached_file :content, styles: { small: "320x240>",medium: "384x288>", large: "640x480>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :content, content_type: /\Aimage\/.*\z/
 end
